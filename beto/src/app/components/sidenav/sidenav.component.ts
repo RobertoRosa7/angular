6488a5b6bd79993c8b7193bcde10edf007f94612
Roberto Rosa <roberto.rosa7@gmail.com>
2019-11-25 17:08:57 -0300

    toggle button added on sidenav

--- beto/src/app/components/sidenav/sidenav.component.ts
+++ beto/src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  @Input('drawer') drawer;
 
+  public listsLinks: any[] = [
+    {
+      home: 'HOME',
+      icon: undefined,
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  public closeSideMenu(){
+    this.drawer.opened = false;
+  }
 }
