1547df3cf5f4c0c644c8d4a664dfd697c7504dd2
Roberto Rosa <roberto.rosa7@gmail.com>
2020-02-01 17:19:25 -0300

    implementacao do request para department e product

--- beto/src/app/components/department/department.component.ts
+++ beto/src/app/components/department/department.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsModel } from 'src/app/models/departments-model';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { MatSnackBar } from '@angular/material';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-department',
@@ -9,24 +13,65 @@ import { DepartmentsModel } from 'src/app/models/departments-model';
 export class DepartmentComponent implements OnInit {
 
   public depName: string;
-  public departments: DepartmentsModel[] = [
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-    {name: 'dep 1', _id: 'ssdjflajfkl'},
-  ];
+  public departments: DepartmentsModel[] = [];
+  public depEidt: DepartmentsModel = null;
+  public unsubscribe$: Subject<any> = new Subject();
 
-  constructor() { }
+  constructor(
+    private depServices: DepartmentsService,
+    private snackbar: MatSnackBar
+  ) { }
 
   ngOnInit() {
+    this.depServices.get()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(deps => (deps) ? this.departments = deps : [])
   }
-
   public save(){
-    console.log('save');
+    if(this.depEidt){
+      this.depServices.edit({name: this.depName, _id: this.depEidt._id})
+        .subscribe(
+            (dep) => {
+              this.notification('departamento atualizado com sucesso.');
+              this.clearField();
+            },
+            (err) => this.notification(err.error)
+        )
+    }else{
+      this.depServices.add({name: this.depName})
+      .subscribe(
+        (dep) => {
+          this.notification('departamento criado com sucesso.')
+          this.clearField();
+        },
+        (err) => this.notification(err.error)
+      )
+    }
   }
   public cancel(){
     console.log('cancel');
   }
+  public edit(dep: DepartmentsModel){
+    this.depName = dep.name;
+    this.depEidt = dep;
+  }
+  public delete(dep: DepartmentsModel){
+    this.depServices.del(dep)
+      .subscribe(
+        () => this.notification('departamento excluído com sucesso.'),
+        (err) => this.notification(err.error)
+      )
+  }
+  public clearField(){
+    this.depName = '';
+    this.depEidt = null;
+  }
+  public notification(msg: string){
+    this.snackbar.open(msg, 'ok', {duration: 3000});
+  }
+
+  public unsubscribe(){
+    this.unsubscribe$.next();
+  }
+
 }
--- beto/src/app/services/departments.service.ts
+++ beto/src/app/services/departments.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, pipe } from 'rxjs';
+import { DepartmentsModel } from '../models/departments-model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentsService {
+  private readonly api = 'http://localhost:8080/v1/department';
+  /**
+   * 
+   * @param http trabalhando com behavior subject para centralização em um único request
+   * assim não será necessário ficar chamando request sempre que for alterado um valor da lista
+   * com isso, podemos centralizar os dados e em todos os ponto que estão sendo imprimido será
+   * alterado sem precisar emitir um evento de atualização dos dados.
+   */
+  private depSubject$: BehaviorSubject<DepartmentsModel[]> = new BehaviorSubject<DepartmentsModel[]>(null);
+  private loaded: boolean = false;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public get(): Observable<DepartmentsModel[]>{
+    if(!this.loaded){
+      this.http.get<DepartmentsModel[]>(this.api)
+        .pipe( tap((deps) => (deps) ? deps.sort((a, b) => { if(a.name > b.name) return 1; if(a.name < b.name) return -1; return 0; }) : []))
+        .subscribe(this.depSubject$);
+      this.loaded = true;
+    }
+    return this.depSubject$.asObservable();
+  }
+  // public get(): Observable<DepartmentsModel[]>{
+  //   return this.http.get<DepartmentsModel[]>(this.api)
+  // }
+  public add(d: DepartmentsModel): Observable<DepartmentsModel>{
+    return this.http.post<DepartmentsModel>(this.api, d)
+
+    // adicionando novos department no subject ordenado pelo nome
+    .pipe( tap((dep: DepartmentsModel) => {
+      this.depSubject$.getValue().push(dep)
+      return this.depSubject$.getValue().sort((a, b) => {if(a.name > b.name) return 1; if(a.name < b.name) return -1;return 0; })
+    }))
+  }
+  public del(d: DepartmentsModel): Observable<any>{
+    return this.http.delete<any>(`${this.api}/${d._id}`)
+    
+    // removendo item do subject e atualizando a lista
+    .pipe( tap(() => {
+      const depVirtualList = this.depSubject$.getValue();
+      const index = depVirtualList.findIndex(id => id._id == d._id);
+      if(index >= 0) depVirtualList.splice(index, 1);
+    }))
+  }
+  public edit(d: DepartmentsModel): Observable<DepartmentsModel>{
+    return this.http.patch<DepartmentsModel>(`${this.api}/${d._id}`, d)
+
+    // atualizando a lista de departamento
+    .pipe( tap((dep) => {
+      const depVirtualList = this.depSubject$.getValue();
+      const index = depVirtualList.findIndex(id => id._id == d._id);
+      if(index >= 0) depVirtualList[index].name = dep.name;
+    }))
+  }
 }
