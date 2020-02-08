d5e737e652dd906a8d26289cbf9f6eec5763324b
Roberto Rosa <roberto.rosa7@gmail.com>
2020-02-08 17:48:37 -0300

    module routing e page not found

--- beto/src/app/app-routing.module.ts
+++ beto/src/app/app-routing.module.ts
@@ -4,18 +4,21 @@ import { HomeComponent } from './page/home/home.component';
 import { ClientsComponent } from './page/clients/clients.component';
 import { DrivenFormsComponent } from './page/driven-forms/driven-forms.component';
 import { StoreComponent } from './page/store/store.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent, children: [
+    path: 'app', component: HomeComponent, children: [
       {path: 'component/:name', component: HomeComponent},
     ]
   },
   {path: 'store', component: StoreComponent},
   {path: 'http-request', component: ClientsComponent},
   {path: 'forms', component: DrivenFormsComponent},
-  {path: '**', redirectTo: '/'}
+
+  {path: '', pathMatch: 'full', redirectTo: 'app'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
